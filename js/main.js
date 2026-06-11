3363ecc77f86287e0a636246ad92e7195752109e
Mariana Pineda <mariana.pineda@grana-t.com>
2026-06-11 18:20:59 -0500

    Landing con video y navbar

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        navbar.style.background = "rgba(255,255,255,0.98)";
+    } else {
+        navbar.style.background = "rgba(255,255,255,0.95)";
+    }
+
+});
